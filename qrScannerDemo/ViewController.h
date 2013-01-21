50905b098313a485a10f27399e56acc94d2f02a9
gmino <intern@gmino.de>
2013-01-21 18:27:22 +0100

    readded zxing framework

--- qrScannerDemo/ViewController.h
+++ qrScannerDemo/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *scanButton;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
---
+++ qrScannerDemo/ViewController.mm
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  qrScannerDemo
+//
+//  Created by Tillmann on 18.01.13.
+//  Copyright (c) 2013 greenmobile. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZXingWidgetController.h"
+#import "QRCodeReader.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
